891e1f691b16cab3079852fb59a6ec1398c38d4a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-10 20:32:21 +0500

    Import and put reducer as an argument in the configure store

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReduce from "../features/todo/todoSlice"
 
-export const store = configureStore({})
+export const store = configureStore({
+    reducer: todoReduce
+})
