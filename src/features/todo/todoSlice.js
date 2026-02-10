4199c5c53f5cdf7599215995738ce0ebd5685f98
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-10 17:18:10 +0500

    Reducers add and remove methods are complete

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,16 +1,22 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    [
-        {id: 1, text: 'Hello World'}
-    ]
+    todos: [{id: 1, text: 'Hello World'}]
 }
 
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: () => {},
-        removeTodo: () => {}
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)   
+        },
+        removeTodo: (state, action) => {
+            state.todos.filter(todo => todo.id !== action.payload.id)
+        }
     }
 })
