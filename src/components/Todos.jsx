81fa5a88dc6ca0ba08184a5a3826229bf5ca0367
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-11 13:54:21 +0500

    Use useSelector hook to get the todos array from state and also make a dispatch variable

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo, updateTodo } from '../features/todo/todoSlice'
 
 function Todos() {
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
   return (
-    <div>Todos</div>
+    <>
+      <div>
+        Todos
+      </div>
+    </>
   )
 }
 
